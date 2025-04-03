f90ed50d5594f440cc383c4a321e0f0f42f2fb7a
Andrea Villaquiran <andreavillaq@gmail.com>
2025-04-03 20:55:54 +0200

    primer commit
    con carpetas y archivos

---
+++ code/script.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def descargar_datos(ruta):
+    df = pd.read_csv(ruta, sep=";")
+    return df
+
+def procesar_fecha(df):
+    df['dt'] = pd.to_datetime(df['dt'])
+    df['year'] = df['dt'].dt.year  
+    return df
+
+if __name__ == "__main__":
+    ruta = r"C:\Users\andri\GitHub\examen_prueba_2025\data\datos_mock.csv"
+    df = descargar_datos(ruta)
+    df = procesar_fecha(df)
+    print(df.head())
